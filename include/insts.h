68e53bb22b4f39a773a6f7a3340d08542438a1ce
luccie-cmd <115068339+luccie-cmd@users.noreply.github.com>
2023-07-29 06:48:23 +0200

    fixed inst mova and movb explanations

--- include/insts.h
+++ include/insts.h
@@ -9,8 +9,8 @@ typedef enum{
     INST_TYPE_STA=0x5a,  // stores the a register into memory with the index of the bytes provided after it
     INST_TYPE_STB=0x5b,  // stores the b register into memory with the index of the bytes provided after it
     INST_TYPE_OBU=0x0b,  // outputs the a register onto the bus
-    INST_TYPE_MOVB=0xba, // moves the b register to the a register
-    INST_TYPE_MOVA=0xab, // moves the a register to the b register
+    INST_TYPE_MOVB=0xba, // moves the a register to the b register
+    INST_TYPE_MOVA=0xab, // moves the b register to the a register
     INST_TYPE_JMP=0x10,  // sets the program index at the provided address (max 255)
     INST_TYPE_LFM=0x90,  // sets the a register to the content of the memory address
     INST_TYPE_NOP=0xea,  // does nothing
